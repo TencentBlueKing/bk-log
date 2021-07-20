f8cb5401d890d03afe7d03e099c9264d7f1126cf
EvildoerXiaoyy <xiaoyyprocess@163.com>
2021-07-20 17:43:29 +0800

    minor: 添加证书及代码重复率修改

--- apps/bk_log_admin/handlers/index_set.py
+++ apps/bk_log_admin/handlers/index_set.py
@@ -45,19 +45,21 @@ class IndexSetHandler(object):
             bk_biz_id=settings.BLUEKING_BK_BIZ_ID,
         )
 
-    def get_date_histogram(self, pk, user_search_history_operation_time):
-        time_zone = get_local_param("time_zone")
-        start_time = int(
-            arrow.get(user_search_history_operation_time["start_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
-        )
-        end_time = int(
-            arrow.get(user_search_history_operation_time["end_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
+    def get_date_histogram(self, index_set_id, user_search_history_operation_time):
+        """
+        @param index_set_id {Int} the id of log_index_set
+        @param user_search_history_operation_time {dict} the search dict
+        @param user_search_history_operation_time.start_time {Str} the search begin
+        @param user_search_history_operation_time.end_time the search end
+        """
+        start_time, end_time = self._get_start_end_time(
+            user_search_history_operation_time=user_search_history_operation_time
         )
 
         daily_data = self._client.custom_metric().query(
             data_name=BK_DATA_CUSTOM_REPORT_USER_INDEX_SET_HISTORY,
             fields=["count(search_history_duration) as _count"],
-            where_conditions=[f"index_set_id = '{pk}'", f"time >= {start_time}", f"time < {end_time}"],
+            where_conditions=[f"index_set_id = '{index_set_id}'", f"time >= {start_time}", f"time < {end_time}"],
             group_by_conditions=[MINUTE_GROUP_BY],
         )
 
@@ -69,18 +71,20 @@ class IndexSetHandler(object):
 
         return {"labels": daily_label_list, "values": daily_data_list}
 
-    def get_user_terms(self, pk, user_search_history_operation_time):
-        time_zone = get_local_param("time_zone")
-        start_time = int(
-            arrow.get(user_search_history_operation_time["start_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
-        )
-        end_time = int(
-            arrow.get(user_search_history_operation_time["end_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
+    def get_user_terms(self, index_set_id, user_search_history_operation_time):
+        """
+        @param index_set_id {Int} the id of log_index_set
+        @param user_search_history_operation_time {dict} the search dict
+        @param user_search_history_operation_time.start_time {Str} the search begin
+        @param user_search_history_operation_time.end_time the search end
+        """
+        start_time, end_time = self._get_start_end_time(
+            user_search_history_operation_time=user_search_history_operation_time
         )
         created_by_data = self._client.custom_metric().query(
             data_name=BK_DATA_CUSTOM_REPORT_USER_INDEX_SET_HISTORY,
             fields=["count(search_history_duration) as _count"],
-            where_conditions=[f"index_set_id = '{pk}'", f"time >= {start_time}", f"time < {end_time}"],
+            where_conditions=[f"index_set_id = '{index_set_id}'", f"time >= {start_time}", f"time < {end_time}"],
             group_by_conditions=["created_by"],
         )
 
@@ -92,20 +96,22 @@ class IndexSetHandler(object):
 
         return {"labels": created_by_label_list, "values": created_by_data_list}
 
-    def get_duration_terms(self, pk, user_search_history_operation_time):
-        time_zone = get_local_param("time_zone")
-        start_time = int(
-            arrow.get(user_search_history_operation_time["start_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
-        )
-        end_time = int(
-            arrow.get(user_search_history_operation_time["end_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
+    def get_duration_terms(self, index_set_id, user_search_history_operation_time):
+        """
+        @param index_set_id {Int} the id of log_index_set
+        @param user_search_history_operation_time {dict} the search dict
+        @param user_search_history_operation_time.start_time {Str} the search begin
+        @param user_search_history_operation_time.end_time the search end
+        """
+        start_time, end_time = self._get_start_end_time(
+            user_search_history_operation_time=user_search_history_operation_time
         )
 
         pie_label_list = []
         pie_data_list = []
         for pie_choice in OPERATION_PIE_CHOICE_MAP:
             pie_label_list.append(pie_choice["label"])
-            where_conditions = [f"index_set_id = '{pk}'", f"time >= {start_time}", f"time < {end_time}"]
+            where_conditions = [f"index_set_id = '{index_set_id}'", f"time >= {start_time}", f"time < {end_time}"]
             if "min" in pie_choice:
                 where_conditions.append(f"search_history_duration >= {pie_choice['min']}")
             if "max" in pie_choice:
@@ -122,10 +128,10 @@ class IndexSetHandler(object):
 
         return {"labels": pie_label_list, "values": pie_data_list}
 
-    def list_user_set_history(self, start_time, end_time, request, view, pk):
+    def list_user_set_history(self, start_time, end_time, request, view, index_set_id):
         time_zone = get_local_param("time_zone")
         user_index_set_history = UserIndexSetSearchHistory.objects.filter(
-            index_set_id=pk,
+            index_set_id=index_set_id,
             is_deleted=False,
             search_type="default",
             created_at__range=[
@@ -171,3 +177,14 @@ class IndexSetHandler(object):
             )
         history["query_string"] = query_string
         return history
+
+    @staticmethod
+    def _get_start_end_time(user_search_history_operation_time):
+        time_zone = get_local_param("time_zone")
+        start_time = int(
+            arrow.get(user_search_history_operation_time["start_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
+        )
+        end_time = int(
+            arrow.get(user_search_history_operation_time["end_time"]).replace(tzinfo=time_zone).float_timestamp * 1000
+        )
+        return start_time, end_time
