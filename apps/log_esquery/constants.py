177e8c09c5f4d0249f0e04c9e07e1fb77084db97
zhutianyu <zxz@zhuxingzhao.com>
2021-08-21 11:15:52 +0800

    feature: 上报ES整体监控数据 (#188)
    * optimization: 去除没有必要的aggs处理代码
    
    * docs: 更新changelog
    
    * bugfix: 修复打包脚本在获取不到lan_ip情况直接报错问题 #50
    
    * docs: 更新changelog
    
    * bugfix：当时间字段说epoch_micros的时候提交报错
    
    * docs: 更新changelog
    
    * bugfix: 修复date_format为000的问题导致无缝解析
    
    * docs: update changelog
    
    * bugfix: 数字数组需要转换成字符串
    
    * docs: changelog update
    
    * feature: trace支持配置grpchost和dataid
    
    * bugfix: aggs接口未兼容修改
    
    * bugfix: 取值错误
    
    * bugfix: binary mode doesn't take an encoding argument
    
    * feature: 增加ES指标数据上报
    
    * feature: es monitor (#185)
    
    * optimization: 去除没有必要的aggs处理代码
    
    * docs: 更新changelog
    
    * bugfix: 修复打包脚本在获取不到lan_ip情况直接报错问题 #50
    
    * docs: 更新changelog
    
    * bugfix：当时间字段说epoch_micros的时候提交报错
    
    * docs: 更新changelog
    
    * bugfix: 修复date_format为000的问题导致无缝解析
    
    * docs: update changelog
    
    * bugfix: 数字数组需要转换成字符串
    
    * docs: changelog update
    
    * feature: trace支持配置grpchost和dataid
    
    * bugfix: aggs接口未兼容修改
    
    * bugfix: 取值错误
    
    * bugfix: binary mode doesn't take an encoding argument
    
    * feature: 增加ES指标数据上报
    
    * bugfix: 时间常量设置
    
    * bugfix: es url params error
    
    * minor: 增加迁移文件 生成dataid
    
    * feature: 增加ip地址维度 and update changelog

--- apps/log_esquery/constants.py
+++ apps/log_esquery/constants.py
@@ -19,6 +19,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 DEFAULT_SCHEMA = "http"
 
-ES_ROUTE_ALLOW_URL = ["_cat", "_cluster", "_nodes"]
+ES_ROUTE_ALLOW_URL = ["_cat", "_cluster", "_nodes", "_stats"]
 
 WILDCARD_PATTERN = "*"
