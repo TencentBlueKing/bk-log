4e2f1b4cb1f9dd64625006edbfb2bff19585cb2a
zzhutianyu <zxz@zhuxingzhao.com>
2021-11-22 09:29:41 +0800

    feature: 聚类事件告警

--- apps/log_measure/events.py
+++ apps/log_measure/events.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making BK-LOG 蓝鲸日志平台 available.
 Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
@@ -16,39 +15,16 @@ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE A
 NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+XXX_EVENT_TYPE = = BK_MONITOR_CLIENT.build_event_trigger(
+    "event_name",
+    data_name=BK_LOG_EVENT_DATA_NAME
+)
+XXX_EVENT_TYPE(content="xxxxx", dimensions={"xxx": "xxxx"})
+or
+XXX_EVENT_TYPE.trigger().set_content("xxx")()
 """
-from bk_monitor_report import MonitorReporter
-from django.apps import AppConfig
-from django.conf import settings
 
-from apps.utils.function import ignored
-from apps.utils.log import logger
+from apps.log_measure.constants import BK_MONITOR_CLIENT, BK_LOG_EVENT_DATA_NAME
 
-
-class MeasureConfig(AppConfig):
-    name = "apps.log_measure"
-    verbose_name = "measure"
-
-    def ready(self):
-        if settings.FEATURE_TOGGLE["monitor_report"] == "off":
-            return
-        with ignored(Exception):
-            from bk_monitor.models import MonitorReportConfig
-
-            from apps.log_measure.constants import DJANGO_MONITOR_DATA_NAME
-
-            monitor_report_config = None
-            try:
-                monitor_report_config = MonitorReportConfig.objects.get(
-                    data_name=DJANGO_MONITOR_DATA_NAME, is_enable=True
-                )
-            except MonitorReportConfig.DoesNotExist:
-                logger.info(f"f{DJANGO_MONITOR_DATA_NAME} data_name init failed")
-                return
-            reporter = MonitorReporter(
-                data_id=monitor_report_config.data_id,
-                access_token=monitor_report_config.access_token,
-                target=settings.APP_CODE,
-                url=f"{settings.BKMONITOR_CUSTOM_PROXY_IP}/v2/push/",
-            )
-            reporter.start()
+CLUSTERING_MONITOR_EVENT = BK_MONITOR_CLIENT.build_event_trigger("clustering_monitor", data_name=BK_LOG_EVENT_DATA_NAME)
