dc7438d8aea0b332bf6b077faf3fcf89bc8c4187
zzhutianyu <zxz@zhuxingzhao.com>
2021-12-13 20:07:05 +0800

    test: fix

--- apps/tests/log_databus/test_e2e_collectorviewsetapi.py
+++ apps/tests/log_databus/test_e2e_collectorviewsetapi.py
@@ -115,6 +115,7 @@ COLLECTORS_LIST = {
                 "deleted_at": None,
                 "deleted_by": None,
                 "custom_type": "log",
+                "custom_name": "容器日志上报",
                 "collector_config_name": "test3333",
                 "bk_app_code": "bk_log_search",
                 "collector_scenario_id": "row",
