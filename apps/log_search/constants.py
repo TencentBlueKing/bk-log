9aa7c492b0c2c300e509df8c82f3bb66947310c2
zzhutianyu <zxz@zhuxingzhao.com>
2021-12-14 14:26:18 +0800

    feature: 采集项列表增加过滤非custom类型采集项

--- apps/log_search/constants.py
+++ apps/log_search/constants.py
@@ -124,6 +124,7 @@ ASYNC_EXPORT_FILE_EXPIRED_DAYS = 2
 ASYNC_EXPORT_EXPIRED = 86400
 HAVE_DATA_ID = "have_data_id"
 BKDATA_OPEN = "bkdata"
+NOT_CUSTOM = "not_custom"
 
 FIND_MODULE_WITH_RELATION_FIELDS = ["bk_module_id", "bk_module_name", "service_template_id"]
 
