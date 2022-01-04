4d2c18a6d99fddf627580171b050d02e3319fe27
ziijiawen <zijialoveplay@qq.com>
2022-01-04 18:52:41 +0800

    minor: global constant

--- web/src/components/collection-access/step.js
+++ web/src/components/collection-access/step.js
@@ -23,63 +23,63 @@
 export const stepsConf = {
   // 采集新增或编辑未完成，itsm
   itsm: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('容量评估'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('容量评估'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
 
   // 采集新增
   add: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 采集修改
   edit: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 采集修改
   editFinish: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 字段提取
   field: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 存储
   storage: [
-    { title: global.mainComponent.$t('采集配置'), icon: '' },
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('btn.Field'), icon: '' },
-    { title: global.mainComponent.$t('dataManage.storage'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集配置'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('btn.Field'), icon: '' },
+    { title: window.mainComponent.$t('dataManage.storage'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 开始采集
   start: [
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
   // 停止采集
   stop: [
-    { title: global.mainComponent.$t('采集下发'), icon: '' },
-    { title: global.mainComponent.$t('完成'), icon: '' },
+    { title: window.mainComponent.$t('采集下发'), icon: '' },
+    { title: window.mainComponent.$t('完成'), icon: '' },
   ],
 };
 
