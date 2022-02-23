3fd42ef19b24a2fc0e53201a170a8c0011acbad1
qqqqqie <1014708380@qq.com>
2022-02-23 14:37:07 +0800

    minor: 模拟单元测试使用缓存的部分

--- scripts/test_env.sh
+++ scripts/test_env.sh
@@ -2,4 +2,3 @@ export BK_PAAS_HOST=https://bk.bktencent.com
 export BKAPP_APIGW_CLOSE=on
 export APP_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 export APP_ID=bk_log
-export BKAPP_USE_REDIS=off
