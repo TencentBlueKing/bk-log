c12660089bfea23a5a1ae78bf14543b9492cd74d
ptaoapeng <1127949533@qq.com>
2022-03-07 19:47:21 +0800

    minor: 容器评估添加滚动条

--- web/src/views/manage/index.vue
+++ web/src/views/manage/index.vue
@@ -103,7 +103,10 @@ export default {
     height: 100%;
 
     .manage-content {
-      height: calc(100% - 52px);
+      // height: calc(100% - 52px);
+      height: 100%;
+      position: relative;
+      top: 51px;
       overflow: auto;
 
       @include scroller($backgroundColor: #C4C6CC, $width: 4px);
