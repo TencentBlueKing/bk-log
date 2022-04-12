0e81a3ecc6c9450f1cabf3364d45ebc1669f8dde
ziijiawen <zijialoveplay@qq.com>
2022-04-12 11:18:16 +0800

    feature: 前端构建优化

--- web/src/views/manage/manage-access/log-collection/collection-item/manage-collection/data-status/select-date.vue
+++ web/src/views/manage/manage-access/log-collection/collection-item/manage-collection/data-status/select-date.vue
@@ -247,7 +247,7 @@ export default {
       width: 220px;
     }
 
-    /deep/ .bk-date-picker-editor {
+    ::v-deep .bk-date-picker-editor {
       background: #fff;
       line-height: 30px;
 
@@ -256,7 +256,7 @@ export default {
       }
     }
 
-    /deep/ .icon-date-picker {
+    ::v-deep .icon-date-picker {
       position: absolute;
       left: 7px;
       top: 7px;
