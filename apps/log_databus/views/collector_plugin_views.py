c45c434332dc7401e47ebe26cdbd2313a4a5dd37
OrenZhang <zhxdida@outlook.com>
2022-05-05 11:49:15 +0800

    minor: 补充View文档

--- apps/log_databus/views/collector_plugin_views.py
+++ apps/log_databus/views/collector_plugin_views.py
@@ -224,6 +224,119 @@ class CollectorPluginViewSet(ModelViewSet):
         @apiName create_collector_plugin_instance
         @apiDescription 实例化采集插件
         @apiGroup 12_CollectorPlugin
+        @apiParam {Int} bk_biz_id 所属业务
+        @apiParam {String} collector_config_name 采集项名称
+        @apiParam {String} collector_config_name_en 采集项英文名称
+        @apiParam {Int} data_link_id 数据链路id
+        @apiParam {String} target_object_type 对象类型，目前固定为 HOST
+        @apiParam {String} target_node_type 节点类型 动态：TOPO  静态：INSTANCE
+        @apiParam {Array[Dict]} target 已选目标
+        @apiParam {Array(json)}  target_nodes 采集目标
+        @apiParam {Int} target_nodes.id 服务实例id （暂时没用到）
+        @apiParam {Int} target_nodes.bk_inst_id 节点实例id (动态)
+        @apiParam {String} target_nodes.bk_obj_id 节点对象id （动态）
+        @apiParam {String} target_nodes.ip 主机实例ip （静态）
+        @apiParam {Int} target_nodes.bk_cloud_id 蓝鲸云区域id （静态）
+        @apiParam {Int} target_nodes.bk_supplier_id 供应商id （静态）
+        @apiParam {String} data_encoding 日志字符集 可选字段`UTF-8, GBK`
+        @apiParam {String} description 备注说明
+        @apiParam {json} params 插件参数（日志路径、过滤方式等）
+        @apiParam {Array} params.paths 日志路径
+        @apiParam {json} params.conditions 过滤方式
+        @apiParam {String} params.conditions.type 过滤方式类型 可选字段 `match, separator`
+        @apiParam {String} params.conditions.match_type 过滤方式 可选字段 `include, exclude`
+        @apiParam {String} params.conditions.match_content 过滤内容
+        @apiParam {String} params.conditions.separator 分隔符
+        @apiParam {String} params.conditions.separator_filters 分隔符过滤条件
+        @apiParam {Array} params.winlog_name windows事件名称
+        @apiParam {Array} params.winlog_level windows事件等级
+        @apiParam {Array} params.winlog_event_id windows事件id
+        @apiParam {String} etl_config 清洗配置
+        @apiParam {Object} etl_params 清洗参数
+        @apiParam {Array} fields 清洗字段
+        @apiParamExample {json} 请求样例:
+        {
+            "bk_biz_id": 706,
+            "collector_config_name": "采集项名称",
+            "collector_config_name_en": "采集项英文名",
+            "data_link_id": 1
+            "target_object_type": "HOST",
+            "target_node_type": "TOPO",
+            "target_nodes": [
+                {
+                   "id": 12
+                },
+                {
+                    "bk_inst_id": 33,   // 节点实例ID
+                    "bk_obj_id": "module",  // 节点对象ID
+                },
+                {
+                    "ip": "127.0.0.1",
+                    "bk_cloud_id": 0,
+                    "bk_supplier_id": 0,
+                }
+            ],
+            "data_encoding": "UTF-8",
+            "description": "这是一个描述",
+            "params": {
+                "paths": ["/log/abc"],
+                "conditions": {
+                    "type": "match",
+                    "match_type": "include",
+                    "match_content": "delete",
+                    "separator": "|",
+                    "separator_filters": [
+                        {
+                            "fieldindex": 1,
+                            "word": "",
+                            "op": "=",
+                            "logic_op": "and"
+                        }
+                    ]
+                },
+                "multiline_pattern": "",
+                "multiline_max_lines": 10,
+                "multiline_timeout": 60,
+                "winlog_name": ["Application", "Security"],
+                "winlog_level": ["info", "error"],
+                "winlog_event_id": ["-200", "123-1234", "123"]
+            },
+            "etl_config": "bk_log_text | bk_log_json | bk_log_regexp | bk_log_delimiter | custom",
+            "etl_params": {
+                "separator_regexp": "[a-z][0-9]",
+                "separator": "|",
+                "retain_original_text": true
+            },
+            "fields": [
+                {
+                    "field_name": "user",
+                    "alias_name": "",
+                    "field_type": "long",
+                    "description": "字段描述",
+                    "is_analyzed": true,
+                    "is_dimension": false,
+                    "is_time": false,
+                    "is_delete": false,
+                },
+                {
+                    "field_name": "report_time",
+                    "alias_name": "",
+                    "field_type": "string",
+                    "description": "字段描述",
+                    "tag": "metric",
+                    "is_analyzed": false,
+                    "is_dimension": false,
+                    "is_time": true,
+                    "is_delete": false,
+                    "option": {
+                        "time_zone": 8,
+                        "time_format": "yyyy-MM-dd HH:mm:ss"
+                    }
+                }
+            ]
+        }
+        @apiSuccess {Int} collector_config_id 采集配置ID
+        @apiSuccess {Int} collector_config_name 采集配置名称
         @apiSuccessExample {json} 成功返回:
         {
             "message": "",
