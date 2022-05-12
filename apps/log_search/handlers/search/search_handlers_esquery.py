e2f8d02e6d5722c62947e95f0ddaa038ad8bff10
zhutianyu <zxz@zhuxingzhao.com>
2022-05-12 16:57:28 +0800

    Bugfix: BCS_CC_APIGATEWAY_ROOT

--- apps/log_search/handlers/search/search_handlers_esquery.py
+++ apps/log_search/handlers/search/search_handlers_esquery.py
@@ -1467,4 +1467,4 @@ class SearchHandler(object):
         raise SearchUnKnowTimeFieldType()
 
     def _enable_bcs_manage(self):
-        return settings.BCS_CC_APIGATEWAY_ROOT if settings.BCS_CC_APIGATEWAY_ROOT != "" else None
+        return settings.BCS_WEB_CONSOLE_DOMAIN if settings.BCS_WEB_CONSOLE_DOMAIN != "" else None
