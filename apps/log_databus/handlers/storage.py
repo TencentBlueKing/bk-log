944dcd82572d3aee11f55d398449be737dc42222
kiritoscs <kiritoscs@gmail.com>
2022-06-08 22:50:15 +0800

    bugfix: 修复集群多业务可见导致采集项不能编辑的bug

--- apps/log_databus/handlers/storage.py
+++ apps/log_databus/handlers/storage.py
@@ -39,6 +39,7 @@ from apps.constants import UserOperationTypeEnum, UserOperationActionEnum
 from apps.iam import Permission, ResourceEnum
 from apps.log_esquery.utils.es_route import EsRoute
 from apps.log_search.models import Scenario, ProjectInfo, BizProperty
+from apps.log_search.handlers.index_set import IndexSetHandler
 from apps.utils.cache import cache_five_minute
 from apps.utils.local import get_local_param, get_request_username
 from apps.api import TransferApi, BkLogApi
@@ -258,6 +259,23 @@ class StorageHandler(object):
                         "source_type": EsSourceType.OTHER.value,
                         "source_name": EsSourceType.get_choice_label(EsSourceType.OTHER.value),
                     }
+                index_sets = IndexSetHandler.get_index_set_for_storage(cluster_obj["cluster_config"]["cluster_id"])
+                if (
+                    cluster_obj["cluster_config"]
+                    .get("custom_option", {})
+                    .get("visible_config", {})
+                    .get("visible_type", "")
+                    == VisibleEnum.MULTI_BIZ.value
+                ):
+                    cluster_obj["cluster_config"]["custom_option"]["visible_config"]["visible_bk_biz"] = [
+                        {
+                            "bk_biz_id": bk_biz_id,
+                            "is_use": index_sets.filter(project_id=projects.get(bk_biz_id), is_active=True).exists(),
+                        }
+                        for bk_biz_id in cluster_obj["cluster_config"]["custom_option"]["visible_config"][
+                            "visible_bk_biz"
+                        ]
+                    ]
                 cluster_obj["cluster_config"]["custom_option"]["bk_biz_id"] = settings.BLUEKING_BK_BIZ_ID
                 cluster_obj["source_type"] = cluster_obj["cluster_config"]["custom_option"]["source_type"]
                 cluster_obj["source_name"] = EsSourceType.get_choice_label(cluster_obj["source_type"])
@@ -279,7 +297,6 @@ class StorageHandler(object):
             cluster_obj["bk_biz_id"] = custom_biz_id
             cluster_obj["source_type"] = custom_option.get("source_type", EsSourceType.OTHER.value)
             cluster_obj["source_name"] = EsSourceType.get_choice_label(cluster_obj["source_type"])
-            from apps.log_search.handlers.index_set import IndexSetHandler
 
             index_sets = IndexSetHandler.get_index_set_for_storage(cluster_obj["cluster_config"]["cluster_id"])
 
