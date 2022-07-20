feacad14fc6bd4bdefab592987159595a934587b
liuwenping <haha.rober@gmail.com>
2022-07-20 11:54:52 +0800

    minor: 新增就绪探测接口，替换之前的healthz接口

--- home_application/urls.py
+++ home_application/urls.py
@@ -42,6 +42,6 @@ urlpatterns = (
     url(r"^bkdata_auth/$", views.bkdata_auth),
     url(r"^contact/$", views.contact),
     url(r"^healthz/$", views.healthz),
-    url(r"^liveness/$", views.liveness),
+    url(r"^liveness/$", views.readiness),
     url(r"^metrics/$", views.metrics),
 )
