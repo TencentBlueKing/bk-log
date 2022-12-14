5fabc7044da46fcc23f3349449dda6659f51ee5b
kiritoscs <kiritoscs@gmail.com>
2022-12-14 12:12:08 +0800

    minor: 只移除bk_cloud_id的默认值

--- apps/log_search/handlers/biz.py
+++ apps/log_search/handlers/biz.py
@@ -247,7 +247,7 @@ class BizHandler(APIModel):
     def get_host_instance_by_ip_list(self, ip_list):
         """
         根据ip获取主机实例
-        :param ip_list: [ip]
+        :param ip_list: [ip, bk_cloud_id]
         :return:
         """
 
@@ -256,8 +256,17 @@ class BizHandler(APIModel):
         # 若有bk_cloud_id,过滤掉host_list中不满足的主机
         filtered_host_list = []
         for ip in ip_list:
-            filtered_host_list.extend([item for item in host_info_list if item["bk_host_innerip"] == ip["ip"]])
-
+            if "bk_cloud_id" in ip.keys():
+                # todo if语句要改
+                filtered_host_list.extend(
+                    [
+                        item
+                        for item in host_info_list
+                        if item["bk_cloud_id"] == ip["bk_cloud_id"] and item["bk_host_innerip"] == ip["ip"]
+                    ]
+                )
+            else:
+                filtered_host_list.extend([item for item in host_info_list if item["bk_host_innerip"] == ip["ip"]])
         biz_to_host = defaultdict(list)
         for host in filtered_host_list:
             biz_to_host[self.bk_biz_id].append(host)
