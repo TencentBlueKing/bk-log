a128ebe34dbd1bef238673f957d00a09894d5d87
qqqqqie <1014708380@qq.com>
2022-12-29 16:37:50 +0800

    feature: transfer checker动态配置

--- apps/log_databus/handlers/check_collector/checker/transfer_checker.py
+++ apps/log_databus/handlers/check_collector/checker/transfer_checker.py
@@ -19,13 +19,17 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 We undertake not to change the open source license (MIT license) applicable to the current version of
 the project delivered to anyone in the future.
 """
+import json
 import logging
+import time
 
+import requests
 from django.conf import settings
 
 from apps.feature_toggle.handlers.toggle import FeatureToggleObject
-from apps.feature_toggle.plugins.constants import CHECK_COLLECTOR_TRANSFER_URL
+from apps.feature_toggle.plugins.constants import CHECK_COLLECTOR_CUSTOM_CONFIG
 from apps.log_databus.constants import EtlConfig, TRANSFER_METRICS, TABLE_TRANSFER
+from apps.log_search.constants import TimeEnum
 from config.domains import MONITOR_APIGATEWAY_ROOT
 from apps.log_databus.handlers.check_collector.checker.base_checker import Checker
 from apps.log_databus.handlers.etl_storage import EtlStorage
@@ -76,33 +80,100 @@ class TransferChecker(Checker):
             self.get_transfer_metric(metric_name)
 
     def get_transfer_metric(self, metric_name: str):
+        app_code = settings.APP_CODE
+        app_secret = settings.SECRET_KEY
         monitor_host = MONITOR_APIGATEWAY_ROOT
 
-        if FeatureToggleObject.switch(CHECK_COLLECTOR_TRANSFER_URL):
-            monitor_host = FeatureToggleObject.toggle(CHECK_COLLECTOR_TRANSFER_URL).feature_config or monitor_host
+        if FeatureToggleObject.switch(CHECK_COLLECTOR_CUSTOM_CONFIG):
+            transfer_custom_config = FeatureToggleObject.toggle(CHECK_COLLECTOR_CUSTOM_CONFIG).feature_config.get(
+                "transfer_custom_config", {}
+            )
+            if not transfer_custom_config:
+                return
+            monitor_host = transfer_custom_config.get("monitor_host")
+            bk_token = transfer_custom_config.get("bk_token")
 
-        bk_monitor_client = Client(
-            bk_app_code=settings.APP_CODE,
-            bk_app_secret=settings.SECRET_KEY,
-            monitor_host=monitor_host,
-            report_host=f"{settings.BKMONITOR_CUSTOM_PROXY_IP}/",
-            bk_username="admin",
-        )
-        params = {
-            "sql": f"select sum({metric_name}) as {metric_name} from {TABLE_TRANSFER} \
-            where time >= '1m' and id == {self.bk_data_id}"
-        }
-        try:
-            result = bk_monitor_client.get_ts_data(data=params)
-            for ts_data in result["list"]:
-                value = ts_data[metric_name]
-                if ts_data["id"] == self.bk_data_id:
-                    self.append_normal_info(f"[请求监控接口] [get_ts_data] {metric_name}: {value}")
+            end_time = int(time.time())
+            start_time = end_time - TimeEnum.FIVE_MINUTE_SECOND.value
+            params_temp = {
+                "down_sample_range": "5s",
+                "start_time": start_time,
+                "end_time": end_time,
+                "expression": "a",
+                "display": True,
+                "query_configs": [
+                    {
+                        "data_source_label": "custom",
+                        "data_type_label": "time_series",
+                        "metrics": [{"field": metric_name, "method": "", "alias": metric_name}],
+                        "table": TABLE_TRANSFER,
+                        "group_by": [],
+                        "display": True,
+                        "where": [
+                            {"condition": "and", "key": "time", "method": "ge", "value": ["1m"]},
+                            {"condition": "and", "key": "id", "method": "eq", "value": [self.bk_data_id]},
+                        ],
+                        "interval": TimeEnum.ONE_MINUTE_SECOND.value,
+                        "interval_unit": "s",
+                        "time_field": "time",
+                        "filter_dict": {},
+                        "functions": [],
+                    }
+                ],
+                "target": [],
+                "bk_biz_id": str(settings.BLUEKING_BK_BIZ_ID),
+            }
+
+            headers = {"Content-Type": "application/json", "Cookie": f"bk_token={bk_token}"}
+
+            try:
+                response = requests.post(
+                    url=monitor_host + "time_series/unify_query/", data=json.dumps(params_temp), headers=headers
+                )
+
+                result = response.json()
+
+                if not result["result"]:
+                    message = f"[请求监控接口] [unify query] 获取 {metric_name} 数据失败, err: {result['message']}"
+                    logger.error(message)
+                    self.append_warning_info(message)
                     return
-            message = f"[请求监控接口] [get_ts_data] 获取 {metric_name} 数据为空"
-            self.append_warning_info(message)
 
-        except Exception as e:
-            message = f"[请求监控接口] [get_ts_data] 获取 {metric_name} 数据失败, err: {e}"
-            logger.error(message)
-            self.append_warning_info(message)
+                series = result["data"].get("series", [])
+                if not series:
+                    message = f"[请求监控接口] [unify query] 获取 {metric_name} 数据为空"
+                    self.append_warning_info(message)
+                    return
+
+                value, _ = series[0]["datapoints"][0]
+                self.append_normal_info(f"[请求监控接口] [unify query] {metric_name}: {value}")
+            except Exception as e:
+                message = f"[请求监控接口] [unify query] 获取 {metric_name} 数据失败, err: {e}"
+                logger.error(message)
+                self.append_warning_info(message)
+        else:
+            bk_monitor_client = Client(
+                bk_app_code=app_code,
+                bk_app_secret=app_secret,
+                monitor_host=monitor_host,
+                report_host=f"{settings.BKMONITOR_CUSTOM_PROXY_IP}/",
+                bk_username="admin",
+            )
+            params = {
+                "sql": f"select sum({metric_name}) as {metric_name} from {TABLE_TRANSFER} \
+                where time >= '1m' and id == {self.bk_data_id}"
+            }
+            try:
+                result = bk_monitor_client.get_ts_data(data=params)
+                for ts_data in result["list"]:
+                    value = ts_data[metric_name]
+                    if ts_data["id"] == self.bk_data_id:
+                        self.append_normal_info(f"[请求监控接口] [get_ts_data] {metric_name}: {value}")
+                        return
+                message = f"[请求监控接口] [get_ts_data] 获取 {metric_name} 数据为空"
+                self.append_warning_info(message)
+
+            except Exception as e:
+                message = f"[请求监控接口] [get_ts_data] 获取 {metric_name} 数据失败, err: {e}"
+                logger.error(message)
+                self.append_warning_info(message)
