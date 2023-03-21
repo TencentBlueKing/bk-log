3117bab3fc0ade3cbada1653d887be5b9af1f338
kiritoscs <kiritoscs@gmail.com>
2023-03-21 16:15:00 +0800

    minor: 项目空间非BKCC关联CC调取API

--- apps/api/modules/utils.py
+++ apps/api/modules/utils.py
@@ -28,6 +28,9 @@ from apps.log_esquery.permission import EsquerySearchPermissions
 from apps.utils import build_auth_args
 from apps.utils.local import get_request
 
+from bkm_space.define import SpaceTypeEnum
+from bkm_space.utils import bk_biz_id_to_space_uid
+
 
 def _clean_auth_info_uin(auth_info):
     if "uin" in auth_info:
@@ -245,3 +248,31 @@ def filter_abnormal_ip_hosts(response_result):
 
     response_result["data"]["info"] = dst_host_list
     return response_result
+
+
+def adapt_non_bkcc(params):
+    # 非CC业务时, 查询关联的CC业务, 如果有, 替换为其关联的CC业务
+    if params.get("bk_biz_id", 0) < 0:
+        from apps.log_search.models import SpaceApi
+
+        space_uid = bk_biz_id_to_space_uid(params["bk_biz_id"])
+        related_space = SpaceApi.get_related_space(space_uid=space_uid, related_space_type=SpaceTypeEnum.BKCC.value)
+        if related_space:
+            params["bk_biz_id"] = related_space.bk_biz_id
+
+    return params
+
+
+def adapt_non_bkcc_for_bknode(params):
+    """
+    适配节点管理的space_id
+    """
+    if params.get("scope", {}).get("bk_biz_id", 0) < 0:
+        from apps.log_search.models import SpaceApi
+
+        space_uid = bk_biz_id_to_space_uid(params["scope"]["bk_biz_id"])
+        related_space = SpaceApi.get_related_space(space_uid=space_uid, related_space_type=SpaceTypeEnum.BKCC.value)
+        if related_space:
+            params["scope"]["bk_biz_id"] = related_space.bk_biz_id
+
+    return params
