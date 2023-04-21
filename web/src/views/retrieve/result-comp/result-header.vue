7d705e44188a7353ce214b8cab90b53832619a8d
v_ptaoapeng <1127949533@qq.com>
2023-04-21 14:42:10 +0800

    minor: 检索页更多显示调整

--- web/src/views/retrieve/result-comp/result-header.vue
+++ web/src/views/retrieve/result-comp/result-header.vue
@@ -101,7 +101,6 @@
         </div>
       </bk-popover>
       <bk-popover
-        v-if="isAiopsToggle"
         trigger="click"
         placement="bottom-end"
         theme="light bk-select-dropdown"
@@ -386,7 +385,7 @@ export default {
      */
     initJumpRouteList(detailStr, isFilterExtract = false) {
       if (!['log', 'es', 'bkdata', 'custom', 'setIndex'].includes(detailStr)) {
-        this.showSettingMenuList = this.settingMenuList;
+        this.showSettingMenuList = this.isAiopsToggle ? this.settingMenuList : [];
         return;
       };
       if (['es', 'bkdata'].includes(detailStr)) {
@@ -395,8 +394,8 @@ export default {
         this.detailJumpRouteKey = 'indexManage';
       } else {
         this.detailJumpRouteKey = detailStr;
-      }
-      this.showSettingMenuList = this.settingMenuList.filter(item => (isFilterExtract ? item.id !== 'extract' : true));
+      };
+      this.showSettingMenuList = this.isAiopsToggle ? this.settingMenuList.filter(item => (isFilterExtract ? item.id !== 'extract' : true)) : [];
       const extraRouteList = this[`${detailStr}DetailKey`].reduce((pre, cur) => {
         pre.push({
           id: cur,
