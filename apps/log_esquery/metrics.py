af0e0c52953796b5d9b76130f86930510dd0daaa
jayjiahua <553544693@qq.com>
2023-04-23 21:12:32 +0800

    minor: 调整统计梯度

--- apps/log_esquery/metrics.py
+++ apps/log_esquery/metrics.py
@@ -10,7 +10,7 @@ ESQUERY_SEARCH_LATENCY = register_metric(
     name="esquery_search_latency",
     documentation="search latency of esquery search API",
     labelnames=("index_set_id", "indices", "scenario_id", "storage_cluster_id", "status"),
-    buckets=(0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 2.5, 5.0, 7.5, 10.0, 20.0, 30.0, 60.0, INF),
+    buckets=(0.05, 0.1, 0.5, 1.0, 2.5, 5.0, 7.5, 10.0, 20.0, 30.0, 60.0, INF),
 )
 
 
